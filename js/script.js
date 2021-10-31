4517bb9f5b6da4bc4aaec4bfa9ced72ba53a6cbe
BlixDyrseth <71275233+BlixDyrseth@users.noreply.github.com>
2021-10-31 23:30:22 +0100

    added products page

--- js/script.js
+++ js/script.js
@@ -1,26 +1,3 @@
-const hiddenMenu = document.getElementById("nav-menu");
-const hamburgerBtn = document.getElementById("hamburgerBtn");
-const closeBtn = document.getElementById("closeBtn");
+import { mobilNav } from "./components/mobilNav.js";
 
-
-hamburgerBtn.onclick = function() {
-    hiddenMenu.style.display = "flex";
-    hamburgerBtn.style.display = "none";
-    closeBtn.style.display = "block"
-    console.log("hello");
-}
-
-closeBtn.onclick = function() {
-    hiddenMenu.style.display = "none";
-    hamburgerBtn.style.display = "block";
-    closeBtn.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if(event.target == hiddenMenu) {
-        hiddenMenu.style.display = "none";
-        hamburgerBtn.style.display = "block"
-        closeBtn.style.display = "none";
-        console.log("Sup?")
-    }
-}
+mobilNav();
